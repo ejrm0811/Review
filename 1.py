4e5337850e1e3f269858c26cf0ecf13659125093
Shin donghyeok <35420463+ejrm0811@users.noreply.github.com>
2018-10-18 21:41:13 +0900

    k

---
+++ 1.py
@@ -0,0 +1,23 @@
+print("2단")
+for i in range(1,10):
+    print(2*i)
+
+print("3단")
+for i in range(1,10):
+    print(3*i)
+
+print("4단")
+for i in range(1,10):
+    print(4*i)
+
+print("5단")
+for i in range(1,10):
+    print(5*i)
+
+print("6단")
+for i in range(1,10):
+    print(6*i)
+
+print("7단")
+for i in range(1,10):
+    print(7*i)
